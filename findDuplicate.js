f556f330c7230516400c07a47c00ed2bd436b756
Dan Politica <73758508+benochi@users.noreply.github.com>
2022-04-13 11:57:50 -0500

    Update findDuplicate.js

--- findDuplicate.js
+++ findDuplicate.js
@@ -1,17 +1,21 @@
-/**
- * @param {number[]} nums
- * @return {number}
+/*Given an array of integers nums containing n + 1 integers where each integer is in the range [1, n] inclusive.
+
+There is only one repeated number in nums, return this repeated number.
+
+You must solve the problem without modifying the array nums and uses only constant extra space.
  */
 var findDuplicate = function(nums) {
     let map = {};
-    //build map object and if value is in map, set to false.
+    //build map object and if value is in map, increment.
     for(let i = 0; i < nums.length; i++){
         if(map[nums[i]]) map[nums[i]] +=1;
         else map[nums[i]] = 1;
     }
-    //search hashmap for false value and return key of false value. 
+    //search hashmap for values greater than 1 and return key of value. 
     for(let [key, value] of Object.entries(map)){
         if(value > 1) return key;
     }
     return -1
 };
+
+//testcases [1,1,2,3,4,5,6] -1 , [2,2,2,2,2,2,2] - 2
