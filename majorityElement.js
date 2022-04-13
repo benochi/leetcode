b6b0988af7ccc0a2bfa852d413ad2a7b1208e0d5
Dan Politica <73758508+benochi@users.noreply.github.com>
2022-04-13 10:52:00 -0500

    Update majorityElement.js

--- majorityElement.js
+++ majorityElement.js
@@ -1,3 +1,15 @@
 //Given an array nums of size n, return the majority element.
 
 //The majority element is the element that appears more than ⌊n / 2⌋ times. You may assume that the majority element always exists in the array.
+//hashmap
+var majorityElement = function(nums) {
+    const majority = Math.round(nums.length / 2)
+    const map = {}
+    for(let i = 0; i<nums.length; i++){
+        // Increment the count of each number in the hashmap
+        map[nums[i]] = (map[nums[i]] || 0) + 1
+        // Return the first number whose count is equal to the majority
+        if(map[nums[i]]===majority) return nums[i];
+    }
+    
+};
