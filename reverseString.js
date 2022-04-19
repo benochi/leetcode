9b217077f055a57b2fd4f206e5386c8a232ef057
Dan Politica <73758508+benochi@users.noreply.github.com>
2022-04-19 11:23:06 -0500

    Update reverseString.js

--- reverseString.js
+++ reverseString.js
@@ -4,3 +4,18 @@
 var reverseString = function(s) {
     s.reverse()
 };
+
+
+//basic
+var reverseString = function(s) {
+  let i = 0;
+  let j = s.length -1;
+    
+  while(i < j){
+    let temp = s[i];
+    s[i]= s[j];
+    s[j] = temp;
+    i++;
+    j--;
+  }
+}
