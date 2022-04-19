8712934c182283cb2c2edf3050e23aab7398222b
Dan Politica <73758508+benochi@users.noreply.github.com>
2022-04-19 11:43:05 -0500

    Create reverseWordsInAString.js

---
+++ reverseWordsInAString.js
@@ -0,0 +1,20 @@
+/*
+Given a string s, reverse the order of characters in each word within a sentence while still preserving whitespace and initial word order.
+Example 1:
+
+Input: s = "Let's take LeetCode contest"
+Output: "s'teL ekat edoCteeL tsetnoc"
+*/
+
+var reverseWords = function(s) {
+  let output =''
+    
+  let words = s.split(' ');
+  for(let i = 0; i < words.length; i++){
+    output += words[i].split('').reverse().join('');
+    if(i < words.length -1){
+      output += " ";
+    }
+  }
+  return output
+};
