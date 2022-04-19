9a0f64f488f22b3286dd694767efd00671ea5b46
Dan Politica <73758508+benochi@users.noreply.github.com>
2022-04-19 12:18:50 -0500

    Create balancedBrackets.js

---
+++ balancedBrackets.js
@@ -0,0 +1,48 @@
+let isBalanced = (input) => {
+
+  let brackets = "[]{}()<>"
+  let stack = []
+
+  for(let bracket of input) {
+    let bracketsIndex = brackets.indexOf(bracket)
+
+    if(bracketsIndex % 2 === 0) {
+      stack.push(bracketsIndex + 1)
+    } else {
+      if(stack.pop() !== bracketsIndex) {
+        return false;
+      }
+    }
+  }
+  return stack.length === 0
+}
+
+console.log(isBalanced('()'))
+> true
+
+console.log(isBalanced('()[]{}<>'))
+> true
+
+console.log(isBalanced('([{<>}])'))
+> true
+
+console.log(isBalanced('([])[{}]{(())}'))
+> true
+
+console.log(isBalanced('('))
+> false
+
+console.log(isBalanced('()]'))
+> false
+
+console.log(isBalanced('([])[{}]{(([))}'))
+> false
+
+console.log(isBalanced('({)'))
+> false
+
+console.log(isBalanced('([)]'))
+> false
+
+console.log(isBalanced('([])[{}]{([)]}'))
+> false
